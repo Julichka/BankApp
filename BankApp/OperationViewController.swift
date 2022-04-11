ef13b7621922b16406654cc334a6060be6477b96
Yuliia Khrupina <julichka1391@gmail.com>
2022-04-10 20:18:25 -0700

    buttons

--- BankApp/OperationViewController.swift
+++ BankApp/OperationViewController.swift
@@ -13,10 +13,69 @@ class OperationViewController: UIViewController {
     var operation: Operation = Operation.GetBalance
     
     @IBOutlet weak var operationLabel: UILabel!
+    @IBOutlet weak var balanceLabel: UILabel!
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet weak var phoneInput: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         displayOperationLabel()
+        
+        switch operation {
+        case .GetBalance:
+            hideViews()
+            showBalanceOnBankDeposit()
+            break
+        case .WithdrawMoney:
+            hideViews()
+            showWithdrawMoneyUi()
+            break
+        case .DepositCash:
+            hideViews()
+            showDepositCashUi()
+            break
+        case .TopUpCellPhone:
+            hideViews()
+            showTopUpCellPhoneUi()
+            break
+        }
+    }
+    
+    @IBAction func onActionButtonClick(_ sender: Any) {
+        
+    }
+    
+    func hideViews() {
+        balanceLabel.isHidden = true
+        input.isHidden = true
+        actionButton.isHidden = true
+        phoneInput.isHidden = true
+    }
+    
+    func showBalanceOnBankDeposit() {
+        balanceLabel.isHidden = false
+        balanceLabel.text = "Balance: $100"
+    }
+    
+    func showWithdrawMoneyUi() {
+        input.isHidden = false
+        actionButton.isHidden = false
+        actionButton.setTitle("Withdraw Money", for: .normal)
+    }
+    
+    func showDepositCashUi() {
+        input.isHidden = false
+        actionButton.isHidden = false
+        actionButton.setTitle("Deposit Cash", for: .normal)
+    }
+    
+    func showTopUpCellPhoneUi() {
+        input.isHidden = false
+        phoneInput.isHidden = false
+        phoneInput.placeholder = "Phone number"
+        actionButton.isHidden = false
+        actionButton.setTitle("Topup cell phone", for: .normal)
     }
     
     func displayOperationLabel() {
@@ -24,12 +83,16 @@ class OperationViewController: UIViewController {
         switch operation {
         case .GetBalance:
             operationName = "displaying the balance on a bank deposit"
+            break
         case .WithdrawMoney:
             operationName = "cash withdrawal from a bank deposit"
+            break
         case .DepositCash:
             operationName = "replenishment of the bank deposit in cash"
+            break
         case .TopUpCellPhone:
             operationName = "replenishment of the phone balance from the deposit"
+            break
         }
         
         operationLabel.text = "Operation: \(operationName)"
